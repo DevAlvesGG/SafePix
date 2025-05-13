af41b01a1c190847a927e60879af60ffe44c9362
Gustavo Alves <gustavodevgarcia@gmail.com>
2025-05-12 21:09:45 -0300

    feat: disponibiliza endpoint HelloWorld

---
+++ src/back/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config() //carrega as variáveis de ambiente do arquivo .env
+const express = require('express')
+const { GoogleGenAI } = require('@google/genai')
+const app = express()
+app.use(express.json()) //para receber JSON no body da requisição
+
+//endpoint: método HTTP, padrãp de acesso, funcionalidades
+//GET, POST, PUT, DELETE
+//usjt.br/notas
+//funcionalidade () => {}
+
+app.post('/pergunte-ao-gemini', async (req, res) => {
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY }) //instancia a classe GoogleGenAI com a chave da API;
+    const prompt = req.body.prompt  //pega o prompt do body da requisição
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: prompt,
+      });
+      res.json({"resposta": response.text})
+})
+
+app.listen(4000, () => console.log('Servidor rodando na porta 4000'))
