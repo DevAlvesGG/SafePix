e53cf61419c3572328c54ebf9965565bd449b292
Gustavo Alves <gustavodevgarcia@gmail.com>
2025-05-13 08:22:08 -0300

    feat: renomeando arquivo e disponibilizando corretamente endpoint hello-word

--- src/back/server.js
+++ src/back/server.js
@@ -9,6 +9,7 @@ app.use(express.json()) //para receber JSON no body da requisição
 //usjt.br/notas
 //funcionalidade () => {}
 
+  //Endpoint POST /pergunte-ao-gemini
 app.post('/pergunte-ao-gemini', async (req, res) => {
     const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY }) //instancia a classe GoogleGenAI com a chave da API;
     const prompt = req.body.prompt  //pega o prompt do body da requisição
@@ -18,5 +19,10 @@ app.post('/pergunte-ao-gemini', async (req, res) => {
       });
       res.json({"resposta": response.text})
 })
+
+//Endpoint GET /hello-word
+app.get('/hello-world', (req, res) => {
+   res.json({message: `Hello World!`})
+});
 
 app.listen(4000, () => console.log('Servidor rodando na porta 4000'))
