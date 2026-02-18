4178153ca9eb3d97d4072b471c814573f15c3a5f
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-02-18 11:52:54 -0300

    feat: cria arquivo para os servicos de denuncia e verificacao de pix

---
+++ src/back/services/pixService.js
@@ -0,0 +1,69 @@
+
+
+const { getDb } = require('../config/database');
+
+async function denouncePixKey(pixKey, denunciaData) {
+    try {
+        const db = getDb();
+        const collection = db.collection('denunciasPix');
+
+        const documentToInsert = {
+            chavePix: pixKey,
+            ...denunciaData,
+            dataDenuncia: new Date()
+        };
+
+        console.log("Tentando inserir documento de denúncia:", documentToInsert);
+        const result = await collection.insertOne(documentToInsert);
+        console.log(`Denúncia registrada com sucesso para a chave Pix ${pixKey}. Inserido ID: ${result.insertedId}`);
+        return result;
+    } catch (error) {
+        console.error('Erro ao denunciar chave Pix:', error);
+        throw error;
+    }
+}
+
+
+async function checkPixKeyStatus(pixKey) {
+    try {
+        const db = getDb();
+        const collection = db.collection('denunciasPix');
+
+        console.log(`Verificando status para chave Pix: ${pixKey}`);
+
+       
+        const count = await collection.countDocuments({ chavePix: pixKey });
+
+        if (count > 0) {
+            
+            const lastDenuncia = await collection.findOne(
+                { chavePix: pixKey },
+                { sort: { dataDenuncia: -1 } } 
+            );
+
+            
+            return {
+                status: 'Denunciada',
+                denuncias: count,
+                motivoUltimaDenuncia: lastDenuncia.motivo,
+                comentarioUltimaDenuncia: lastDenuncia.comentario,
+                ultimaDenuncia: lastDenuncia.dataDenuncia,
+                nome: lastDenuncia.nome, 
+                sobrenome: lastDenuncia.sobrenome,
+                dataNascimento: lastDenuncia.dataNascimento, 
+                genero: lastDenuncia.genero 
+            };
+        } else {
+            
+            return { status: 'Não Denunciada' };
+        }
+    } catch (error) {
+        console.error('Erro ao verificar status da chave Pix:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    denouncePixKey,
+    checkPixKeyStatus
+};
