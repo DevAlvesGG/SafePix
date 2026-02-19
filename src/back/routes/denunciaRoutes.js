5f0da538dc3c5d7839bd039efdc8edfbf9ac7b8b
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-02-19 15:26:47 -0300

    feat: cria arquivos de rotas para verificar e denunciar pix

--- src/back/routes/denunciaRoutes.js
+++ src/back/routes/denunciaRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // importamos o controller que já tem as regras de validação/serviço
-const { criarDenuncia, verificarStatusPix } = require('../controllers/pixController');
+const pixController = require('../controllers/pixController');
 
 // POST /api/denunciarPix
-router.post('/denunciarPix', criarDenuncia);
+router.post('/denunciarPix', pixController.criarDenuncia);
 
 // GET /api/verificarPix/:chavePix
-router.get('/verificarPix/:chavePix', verificarStatusPix);
+router.get('/verificarPix/:chavePix', pixController.verificarStatusPix);
 
 module.exports = router;
