2c0304224374e2265e720e359f4a7030a7c1014e
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-03-23 16:50:24 -0300

    feat(userModel): cria funcao que busca usuario por email

--- src/back/models/userModel.js
+++ src/back/models/userModel.js
@@ -1,6 +1,7 @@
 // devo importar minha conexao com o mongodb
 const db = require('../config/database')
 const { BSON: { ObjectId } } = require('mongodb');
+const bcrypt = require('bcrypt')
 
 const userModel = {
 
@@ -12,6 +13,36 @@ const userModel = {
             return result;
         } catch (error) {
             throw new Error(`Erro ao Retornar Usuários: ${error.message}`)
+        }
+    },
+
+    //create new user
+    createUser: async (email, password) => {
+        try {
+            const database = db.getDb();
+
+            //valida o tipo das credenciais 
+            if(typeof email !== 'string' || typeof password !== 'string') throw new Error('Credenciais Precisam Ser String');
+
+            //valida se email ja esta cadastrado
+            const existingUser = await database.collection('users').findOne({ email });
+            if(existingUser) {
+                throw new Error('Email já cadastrado.');
+            }
+
+            //criptografa a password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const user = await database.collection('users').insertOne({
+                email: email,
+                password: hashedPassword,
+                createdAt: new Date()
+            });
+
+            return user;
+            
+        } catch (error) {
+            throw new Error(`Erro ao Criar Usuário: ${error.message}`)
         }
     },
 
@@ -64,6 +95,21 @@ const userModel = {
 
         } catch (error) {
             throw new Error(`Erro ao Deletar Usuário: ${error.message}`)
+        }
+    },
+
+    //return user by email
+    findUserByEmail: async (email) => {
+        try {
+            const database = db.getDb();
+            const user = await database.collection('users').findOne({email: email})
+
+            if (!user) throw new Error('E-mail Não Encontrado.')
+
+            return user;
+
+        } catch (error) {
+            
         }
     }
 }
