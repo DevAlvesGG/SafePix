84ff55db6c49eb244a07f5bb7934bc50afc25ccd
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-03-30 08:32:30 -0300

    chore: refatora o codigo e nome do arquivo

---
+++ src/back/routes/complaintRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// importamos o controller que já tem as regras de validação/serviço
+//const pixController = require('../controllers/pixController');
+
+// POST /api/denunciarPix
+//router.post('/denunciarPix', pixController.criarDenuncia);
+
+// GET /api/verificarPix/:chavePix
+//router.get('/verificarPix/:chavePix', pixController.verificarStatusPix);
+
+module.exports = router;
