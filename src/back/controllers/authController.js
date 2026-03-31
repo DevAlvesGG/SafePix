7961e1268a27cea1bd1ac5737530c51c7af002eb
GUSTAVO ALVES <gustavo.alves@HISPAGNOL.CORP>
2026-03-31 10:43:48 -0300

    feat: refatora authController para melhor identificacao de erros e implementa autenticacao com jwt

--- src/back/controllers/authController.js
+++ src/back/controllers/authController.js
@@ -4,34 +4,39 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     //GET /auth/login
-    login: (req, res) => {
-        const { email, password } = req.params;
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body;
 
-        //valido se o user existe
-        const existingUser = userModel.findUserByEmail(email);
+            //valido se o user existe
+            const existingUser = await userModel.findUserByEmail(email);
+            if (!existingUser) {
+                res.status(400).json({ message: 'Usuário Não Cadastrado'});
+            }
 
-        if (!existingUser) {
-            res.status(400).json({ message: 'Usuário Não Cadastrado'})
+            //compara se a senha criptografada é igual a senha do usuario
+            const validPassword = bcrypt.compareSync(password, existingUser.password)
+            if (!validPassword)  {
+                res.status(400).json({ message: 'Usuário ou Senha Inválida'})
+            }
+
+            const payload = { email: existingUser.email, id: existingUser.id };
+            const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn: '1d'} );
+            res.json(token);
+
+        } catch (error) {
+            res.status(500).json({ message: `Erro ao Fazer Login: ${error.message}`})
         }
-
-        //compara se a senha criptografada é igual a senha do usuario
-        const validPassword = bcrypt.compareSync(password, existingUser.password)
-
-        if (!validPassword)  {
-            res.status(400).json({ message: 'Usuário ou Senha Inválida'})
-        }
-
-        const payload = { email: existingUser.email, id: existingUser.id };
-        const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn: '1d'} );
-
-        res.jso(token);
     },
 
     //POST /auth/register
-    register: (req, res) => {
-        const { email, password } = req.params;
-        const newUser = userModel.createUser(email, password);
-
-        return res.status(201).json(newUser);
+    register: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const newUser = await userModel.createUser(email, password);
+            return res.status(201).json(newUser);
+        } catch (error) {
+            return res.status(500).json({ message: `Erro ao Cadastrar Usuário: ${error.message}`})
+        }  
     }
 }
