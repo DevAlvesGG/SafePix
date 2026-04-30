47f582769f317efad87b76bb70f7b3f288bbe951
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-04-30 20:40:33 -0300

    feat: refactor complaintController

--- src/back/controllers/complaintController.js
+++ src/back/controllers/complaintController.js
@@ -1,76 +1,69 @@
-const { moduleRunnerTransform } = require('vite');
-const { denouncePixKey, checkPixKeyStatus } = require('../services/pixService');
-
-const { validarDenuncia } = require('../validators/denunciaValidator');
-
+const { createComplaint, checkPixKey, deleteComplaint } = require('../models/complaintModel');
 
 module.exports = {
 
 // POST /api/denunciarPix -- create new complaint
-    createComplaint: (req, res) => {
+    createComplaintPix: async (req, res) => {
         try {
             const { chavePix, nome, sobrenome, dataNascimento, genero, motivo, comentario } = req.body;
-
-            res.status(201).json({ message: 'Denuncia registrada com Sucesso!', data: result});
+            const complaintData = { 
+                //Em JavaScript moderno, quando a chave e a variável têm o mesmo nome, você pode simplificar, Isso se chama shorthand property
+                chavePix,
+                nome,
+                sobrenome,
+                dataNascimento,
+                genero,
+                motivo,
+                comentario
+            };
+            const create = await createComplaint(complaintData);
+            if(!create) {
+                return res.status(400).json({ message: 'Erro ao criar denúncia.' });
+            }
+            res.status(201).json({ message: 'Denuncia registrada com Sucesso!', data: create});
 
         } catch (error) {
-            throw new Error(`Erro ao criar denúncia: ${error.message}`);
+            if(error.message.includes('inválidos')) {
+                return res.status(400).json({ message: error.message });
+            }
+            res.status(500).json({ message: 'Erro interno do servidor ao criar denuncia.', error: error.message})
         }
-    }
+    },
 
+    // GET /api/verificarPix/:chavePix -- check complaint status by Pix key
+    checkStatusPix: async(req, res) => {
+        try {
+            const { chavePix } = req.params;
 
-}
-// POST /api/denunciarPix
-async function criarDenuncia(req, res) {
-    try {
-        const { chavePix, nome, sobrenome, dataNascimento, genero, motivo, comentario } = req.body;
+            //validação simples para garantir que a chave Pix foi fornecida
+            if(!chavePix || chavePix.trim() === '') {
+                return res.status(400).json({ message: 'Chave Pix é obrigatória para verificações.'});
+            }
 
-        //valida os dados da denuncia usando a função de validação
-        const validacao = validarDenuncia({
-            chavePix,
-            nome,
-            motivo,
-        });
+            const statusPix = await checkPixKey(chavePix);
+            res.status(200).json(statusPix);
+            
+        } catch (error) {
+            console.error('Erro em verificar status da chave Pix: ', error);
 
-        //se não passou na validacao, retorna os erros encontrados
-        if(!validacao.valid) {
-            return res.status(400).json({ message: 'Dados inválidos', errors: validacao.errors
-            })
+            res.status(500).json({ message: 'Erro interno do servidor ao verificar o status da chave Pix.', error: error.message});
         }
-        //se passou da validacao, tenta criar a denuncia
-        const result = await denouncePixKey(chavePix, {
-            nome,
-            sobrenome,
-            dataNascimento,
-            genero,
-            motivo,
-            comentario
-        })
+    },
 
-        res.status(201).json({ message: 'Denuncia registrada com Sucesso!', data: result});
-
-    } catch (error) {
-        console.error('Erro em criar denuncia:', error);
-        res.status(500).json({ message: 'Erro interno do servidor ao processar a denúncia.', error: error.message });
-    }
-}
-
-async function verificarStatusPix(req, res) {
-    try {
-        const { chavePix } = req.params;
-
-        //validação simples para garantir que a chave Pix foi fornecida
-        if(!chavePix || chavePix.trim() === '') {
-            return res.status(400).json({ message: 'Chave Pix é obrigatória para verificações.'});
+    // DELETE /api/excluirDenuncia/:id
+    deleteComplaintPix: async (req, res) => {
+        try {
+            const { id } = req.params;  
+            // Validação simples para garantir que a chave Pix foi fornecida
+            if (!id) {
+                return res.status(400).json({ message: 'ID é obrigatório para exclusão.' });
+            }  
+            const deleteResult = await deleteComplaint(id)
+            res.status(200).json({ message: 'Denúncia excluída com sucesso.', data: deleteResult });
+        } catch (error) {
+            res.status(500).json({ message: 'Erro interno do servidor ao excluir denúncia.', error: error.message });
         }
 
-        const statusPix = await checkPixKeyStatus(chavePix);
-        res.status(200).json(statusPix);
-        
-    } catch (error) {
-        console.error('Erro em verificar status da chave Pix: ', error);
-
-        res.status(500).json({ message: 'Erro interno do servidor ao verificar o status da chave Pix.', error: error.message});
     }
-}
+};
 
